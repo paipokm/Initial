54243adecc7e6170d82e2107d43ee34cbb2a542a
paipokm <40974134+paipokm@users.noreply.github.com>
2018-07-09 04:53:24 +0000

    I wonder how to use GitHub...

---
+++ Sample.py
@@ -0,0 +1,14 @@
+name=" main "
+
+if name == " main ":
+    print("スクリプト実行時に実行されるよ")
+a=2
+
+if a%15==0:
+    print("FizzBuzz")
+elif a%5==0:
+    print("Buzz")
+elif a%3==0:
+    print("Fizz")
+else:
+    print(a)
